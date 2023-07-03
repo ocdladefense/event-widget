fe867af67d7a22b74274ce9a7224439640eddeb2
Lewisic <Lewisic@my.lanecc.edu>
2023-07-03 15:29:53 -0700

    Finished feature D

--- data.js
+++ data.js
@@ -1,45 +1,4 @@
-let event1 = {
-    'summary': '4th of July',
-    'location': 'Park',
-    'description': 'Fireworks in the park!',
-    'start': {
-        'date': '2023-07-04'
-    },
-    'end': {
-        'date': '2023-07-04'
-    },
-    'status': 'confirmed',
-    'attendees': [
-        { 'email': 'king1@middleearth.com' },
-        { 'email': 'king2@middleearth.com' }
-    ],
-    'reminders': {
-        'useDefault': true
-    }
-};
-let event2 = {
-    'summary': 'Intern Meeting',
-    'location': 'Zoom',
-    'description': 'Join in to talk about your internship',
-    'start': {
-        'dateTime': '2023-06-30T10:00:00-07:00',
-        'timeZone': 'America/Los_Angeles'
-    },
-    'end': {
-        'dateTime': '2023-06-30T12:00:00-07:00',
-        'timeZone': 'America/Los_Angeles'
-    },
-    'attendees': [
-        { 'email': 'intern@gmail.com' },
-        { 'email': 'intern2@gmail.com' }
-    ],
-    'reminders': {
-        'useDefault': true
-    }
-};
 
-
-const events = [event1, event2];
 
 
 
--- dev_modules/http/Url.js
+++ dev_modules/http/Url.js
@@ -3,7 +3,7 @@ class Url {
     constructor(url) {
         this.url = url;
     }
-
+    //https://www.googleapis.com/calendar/v3/calendars/biere-library@thebierelibrary.com/events?timeMin=2023-07-01&timeMax=2023=07-15&test
     parseQueryString() {
         let parts = this.url.split("?");
         let queryString = parts[1];
