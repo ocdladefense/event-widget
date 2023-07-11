6f6e1d405f5f50af4c0b6b0fc2ae72b46c3bf6fb
Zaelin McCarty <thezealot35@gmail.com>
2023-07-11 13:02:41 -0700

    Custom event element

--- controller.js
+++ controller.js
@@ -4,15 +4,14 @@ import { HttpCache } from "./dev_modules/http/HttpCache.js";
 import { GoogleApisCalendarMock } from "./dev_modules/mock/GoogleApisCalendarMock.js";
 import { Url } from "./dev_modules/http/Url.js";
 import { ISODate } from "./dev_modules/date/ISODate.js";
-export default init;
-export { init };
+export { init, Events };
 
 
 
 
 function queryByDateRange(start = null, end = null) {
     // built-ins
-    let calendarId = "biere-library@thebierelibrary.com";
+    let calendarId = "events@thebierelibrary.com";
     let url = "https://www.googleapis.com/calendar/v3/calendars/" + calendarId + "/event?fu=bar";
     url = new Url(url);
     //url = url + "?" + "timeMin=" + start + "&timeMax=" + end;
@@ -38,6 +37,92 @@ const env = {
     displayErrors: true, // We can imagine sceniors where we might *want to dipslay a message to the user.
 };
 
+class Events extends HTMLElement {
+
+    startDate = null;
+    endDate = null;
+
+    constructor() {
+        super();
+
+        this.calendarId = this.getAttribute("calendar-id");
+
+        if (this.calendarId == null) {
+            console.error("No calendar ID specified.");
+        }
+
+        this.startDate = this.getAttribute("start-date") || env.today;
+        this.endDate = this.getAttribute("end-date") || env.today;
+    }
+    
+    // Called each time the element is appended to the window/another element
+    async connectedCallback() {
+        const shadow = this.attachShadow({ mode: "open" });
+
+        const list = document.createElement("div");
+
+        this.list = list;
+        
+        this.shadowRoot.append(list);
+
+        let url = queryByDateRange(this.startDate, this.endDate);
+        HttpClient.register("www.googleapis.com", new GoogleApisCalendarMock());
+
+        const req = new Request(url);
+
+        const resp = await client.send(req);
+        //wasn't working like this so went back to old way temporarily 
+        //await client.send(req)
+        resp.json()
+            .then(events => {
+
+                if (events.error) {
+                    throw new Error(events.message, { cause: events });
+                }
+                console.log(events);
+
+                this.list.innerHTML = renderEvents(events).join("\n");
+            })
+            .catch(error => {
+                // alert('Error: ' + error.message);
+                console.error(error);
+                if (env.displayErrors && error.cause.code == "RANGE_EMPTY") { // Might help the customer.
+                    this.list.innerHTML = "Free to Register";
+                }
+            });
+    }
+    /*
+    onEventClick() {
+        let url = queryByDateRange(this.startDate, this.endDate);
+
+        const eventsContainer = document.getElementById('events');
+        const req = new Request(url);
+
+        const resp = await client.send(req);
+        //wasn't working like this so went back to old way temporarily 
+        //await client.send(req)
+        resp.json()
+            .then(events => {
+
+                if (events.error) {
+                    throw new Error(events.message, { cause: events });
+                }
+                console.log(events);
+
+                eventsContainer.innerHTML = renderEvents(events).join("\n");
+            })
+            .catch(error => {
+                // alert('Error: ' + error.message);
+                console.error(error);
+                if (env.displayErrors && error.cause.code == "RANGE_EMPTY") { // Might help the customer.
+                    eventsContainer.innerHTML = "Free to Register";
+                }
+            });
+    }
+    */
+
+
+}
 
 async function init() {
     let url = queryByDateRange("2023-07-01", "2023-07-15");
